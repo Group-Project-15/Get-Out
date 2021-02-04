b743e9daa489de320580624ef63e71c06b967f24
ArdianChandra <sensenkac@gmail.com>
2021-02-04 20:06:32 +0700

    betulin authentic

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,25 +1,15 @@
 const { User } = require('../models')
 const { checkToken } = require('../helpers/jwt')
-
+const jwt = require("jsonwebtoken")
 
 function authenticate(req, res, next) {
     try {
-      const decode = checkToken(req.headers.access_token)
-      const email = decode.email
-      User.findOne({
-        where: {email}
-      })
-      .then(data => {
-        if(data) {
-          req.user = {
-            id: data.id,
-            email: data.email
-          }
-          next()
-        } else res.status(401).json({msg: `User not Authorized`})
-      })
+      let access_token = req.headers.access_token
+      let decode = checkToken(access_token)
+      req.decode = decode
+      next()
     } catch (err) {
-        res.status(400).json({msg: `Data not found`})
+      next({name: "custom", msg: "Invalid token", status: 401})
     }
 }
 
