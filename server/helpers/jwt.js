d18c711b2083332429cc061a488933e4f0e00ed8
Zalfa <zalfa.muhammad@gmail.com>
2021-02-04 16:22:25 +0700

    jwtoken

---
+++ server/helpers/jwt.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+const secretKey = process.env.JWT_KEY
+
+function generateToken(payload) {
+    return jwt.sign(payload, secretKey)
+}
+
+function checkToken(access_token) {
+    return jwt.verify(access_token, secretKey)
+}
+
+module.exports = {
+    generateToken,
+    checkToken
+}
