696d8829c5330f8e6a749844b67d2e863fe90dfb
okipratama97 <okipratama97@gmail.com>
2021-02-04 22:31:38 +0700

    api-all

--- server/controllers/informationController.js
+++ server/controllers/informationController.js
@@ -70,6 +70,45 @@ class InformationController {
         next(err)
       })
   }
+
+  static fetchWeather(req, res, next) {
+    const location = req.decode.location || "Jakarta"
+    const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.OPEN_WEATHER_APIKEY}`
+    axios
+      .get(apiURL)
+      .then(response => {
+        const localweather = {
+          weather: response.data.weather[0].main,
+          temp: response.data.main.temp,
+          city: response.data.name
+        }
+
+        res.status(200).json(localweather)
+      })
+      .catch(err => {
+        next(err)
+      })
+  }
+
+  static fetchNews(req, res, next) {
+    const apiURL = `https://api.currentsapi.services/v1/latest-news?country=ID&apiKey=${process.env.CURRENTS_APIKEY}`
+    axios
+      .get(apiURL)
+      .then(response => {
+        const news = response.data.news.slice(0, 3).map(el => {
+          return {
+            title: el.title,
+            published: el.published,
+            url: el.url,
+            image: el.image
+          }
+        })
+        res.status(200).json(news)
+      })
+      .catch(err => {
+        next(err)
+      })
+  }
 }
 
 module.exports = InformationController
