546ad3fb1cb930af8d65dedc880092fd3338cc73
ArdianChandra <sensenkac@gmail.com>
2021-02-04 18:38:10 +0700

    bisa regis, dan login sudah dapet token

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
+require("dotenv").config()
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes/index')
 const app = express()
 const port = 3000
-
+const errorHandler = require("./middlewares/errorHandler")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(cors())
 app.use(router)
 
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`listen in port ${port}`);
--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken')
-const secretKey = process.env.JWT_KEY
+// const secretKey = process.env.JWT_KEY
+// console.log(process.env.SECRET);
 
 function generateToken(payload) {
-    return jwt.sign(payload, secretKey)
+    return jwt.sign(payload, "secretKey")
 }
 
 function checkToken(access_token) {
-    return jwt.verify(access_token, secretKey)
+    return jwt.verify(access_token, "secretKey")
 }
-
+  
 module.exports = {
     generateToken,
     checkToken
